2fe1798385465cecec0f8a94ea24a5c5fb9be466
ScriptedChicken <angusfhunt@gmail.com>
2023-12-09 09:02:56 +1300

    Added ChatGPT functionality

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException, TimeoutException, NoSuchWindowException
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from openai import OpenAI
 
 
 class QuickApplyException(Exception):
@@ -32,6 +33,12 @@ def wait_random():
     seconds = random.randint(1, 3)
     print(f"Waiting {seconds} seconds", end='\r')
     time.sleep(seconds)
+
+
+def return_api_key():
+    with open('secrets.json', 'r') as file:
+        secrets = json.load(file)
+    return secrets["chat_gpt_login"]["key"]
 
 
 def log_in():
@@ -61,6 +68,16 @@ def save_visited_job(job_id, data_dict):
     visited_jobs[job_id] = data_dict
     with open('visited_jobs.json', 'w') as file:
         json.dump(visited_jobs, file, indent=4)
+
+
+def return_chat_gpt_response(job_description):
+    client = OpenAI(api_key=return_api_key())
+    message = f"In no more that 70 words, write why I want this job based on this job description: {job_description}"
+    chat_completion = client.chat.completions.create(
+        messages=[{"role": "user","content": message}],
+        model="gpt-3.5-turbo",
+    )
+    return chat_completion.choices[0].message.content
 
 
 role = "developer"
@@ -101,11 +118,18 @@ for listing in res.json().get('data'):
         pass
     
     else:
+        driver.get(job_url)
         replacements = {
             "COMPANY_NAME": company_name,
             "JOB_TITLE": job_title,
-            "LOCATION_NAME": location,
+            "LOCATION_NAME": location
         }
+
+        if role == "chat_gpt_test":
+            time.sleep(2)
+            element = driver.find_element(By.CSS_SELECTOR, 'div[data-automation="jobAdDetails"]')
+            chat_gpt_response = return_chat_gpt_response(element.text)
+            replacements["CHAT_GPT_RESPONSE"] = chat_gpt_response
     
         if is_agency:
             template_path = os.path.join("templates", f"cover_letter_angus_hunt_{role}_agency.docx")
@@ -137,8 +161,6 @@ for listing in res.json().get('data'):
         cv_temp = os.path.join(TEMP_PATH, cv_name)
         shutil.copyfile(cv_path, cv_temp)
 
-        driver.get(job_url)
-
         try:
             already_applied_flag = driver.find_element(By.ID, 'applied')
             print("Already applied - skipping")
@@ -167,7 +189,8 @@ for listing in res.json().get('data'):
             data_dict["applied"] = True
 
         save_visited_job(job_id, data_dict)
-        os.remove(cover_letter_temp)
-        os.remove(cv_temp)
+        # os.remove(cover_letter_temp)
+        # os.remove(cv_temp)
+        break
 
 print("Complete")
--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url = r"https://www.seek.co.nz/api/chalice-search/v4/search?siteKey=NZ-Main&where=All+Australia&keywords=Python+Developer"
-res = requests.get(url)
-print(res.status_code)
-
-with open('listings.json', 'w') as file:
-    json.dump(res.json().get('data'), file, indent=4)
