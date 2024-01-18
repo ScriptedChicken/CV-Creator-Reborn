d1751c7e1f5e16a6c17f251afa5779d6bc25a5f0
ScriptedChicken <angusfhunt@gmail.com>
2024-01-18 22:43:40 +1300

    Reworked core functionality, added handling multiple ChatGPT prompts, added event loop

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import sys
+import re
 from docx import Document
 import shutil
 import time
@@ -10,21 +11,21 @@ import pyperclip
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchWindowException, TimeoutException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from openai import OpenAI
 import tkinter as tk
 from tkinter import ttk, filedialog, NORMAL, DISABLED
 import threading
-from urllib3.exceptions import MaxRetryError
 
 
 class Seeker(object):
-    def __init__(self, role, temp_path, where, keywords, view_previous_jobs):
+    def __init__(self, name, role, temp_path_dict, where, keywords, view_previous_jobs):
+        self.name = name
         self.driver = None
         self.role = role
-        self.temp_path = temp_path
+        self.temp_path_dict = temp_path_dict
         self.where = where
         self.keywords = keywords
         self.view_previous_jobs = view_previous_jobs
@@ -52,7 +53,7 @@ class Seeker(object):
         self.temporary_documents = []
         self.listings_array = []
         self.listings_iterator = None
-        
+
     @staticmethod
     def return_clean_name(name):
         return name.lower().replace(" ", "_").replace("-", "_").replace("/", "_").replace("|", "_").replace("__", "_")
@@ -68,7 +69,7 @@ class Seeker(object):
         time.sleep(seconds)
     
     def log_in(self):
-        self.load_secrets()
+
         email = self.secrets['seek_login']['email']
         password = self.secrets['seek_login']['password']
     
@@ -97,24 +98,26 @@ class Seeker(object):
         self.visited_jobs[self.job_id] = self.data_dict
         with open('visited_jobs.json', 'w') as file:
             json.dump(self.visited_jobs, file, indent=4)
-            print("Saved")
-    
-    def return_chat_gpt_response(self, job_description):
+
+    def return_chat_gpt_response(self, query, job_description):
         client = OpenAI(api_key=self.secrets["chat_gpt_login"]["key"])
-        message = f"In no more that 70 words, write why I want this job based on this job description: {job_description}"
+        message = f"{query}: {job_description}"
         chat_completion = client.chat.completions.create(
             messages=[{"role": "user", "content": message}], model="gpt-3.5-turbo",
         )
         return chat_completion.choices[0].message.content
+
+    def set_document_name(self):
+        self.document_name = self.return_clean_name(f"{self.document_type}_{self.name}_{self.job_title}.docx")
         
-    def save_document(self, document_type):
-        document_name = self.return_clean_name(f"{document_type}_angus_hunt_{self.job_title}.docx")
-        document_path = os.path.join(self.output_folder, document_name)
-        self.document.save(document_path)
-        document_temp_path = os.path.join(self.temp_path, document_name)
-        shutil.copyfile(document_path, document_temp_path)
-        self.temporary_documents.append(document_temp_path)
-        
+    def save_document(self):
+        document_output_path = os.path.join(self.output_folder, self.document_name)
+        self.document.save(document_output_path)
+
+        document_temporary_path = os.path.join('temporary', self.document_name)
+        shutil.copyfile(document_output_path, document_temporary_path)
+        self.temporary_documents.append(document_temporary_path)
+
     def update_document(self):
         for key, value in self.replacements.items():
             for paragraph in self.document.paragraphs:
@@ -133,15 +136,13 @@ class Seeker(object):
                     paragraph.text = paragraph.text.replace(key, value)
 
     def remove_temporary_files(self):
+        print("Removing temporary files")
         for temporary_document in self.temporary_documents:
             try:
                 os.remove(temporary_document)
             except:
-                print(f"Couldn't remove {temporary_document}")
+                pass
         
-    def query_chat_gpt(self):
-        self.replacements["CHAT_GPT_RESPONSE"] = self.return_chat_gpt_response(self.job_details)
-
     def click_quick_apply(self):
         quick_apply_button = WebDriverWait(self.driver, 2).until(
             EC.presence_of_element_located((By.LINK_TEXT, 'Quick apply'))
@@ -158,36 +159,56 @@ class Seeker(object):
             self.listings_array.extend(res.json().get('data'))
         self.listings_iterator = iter(self.listings_array)
 
+    def check_state(self):
+        self.root.update()
+
+        if self.skip_clicked:
+            print(f"Skip clicked: {self.skip_clicked}")
+            self.skip_clicked = False
+            self.save_visited_job()
+            self.remove_temporary_files()
+            self.restart_thread()
+
+        try:
+            application_sent = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.ID, 'applicationSent')))
+            self.messages_label.config(text=f"Application for {self.job_title} sent successfully.")
+            self.remove_temporary_files()
+            self.apply()
+            self.data_dict["applied"] = True
+        except:
+            pass
+
+        self.root.after(1000, self.check_state)
+
     def complete_application(self):
         if EC.presence_of_element_located((By.ID, 'password')):
             try:
                 self.log_in()
             except:
-                print("Automatic log-in failed; please enter login details manually")
+                pass
 
-        pyperclip.copy(self.temp_path)
+        pyperclip.copy(self.temp_path_dict[self.document_type])
+        self.messages_label.config(text="Please complete application.")
 
-        print("Please complete application. This browser will expire in 5 mins.")
 
-        try:
-            WebDriverWait(self.driver, 300).until(
-                EC.presence_of_element_located((By.ID, 'applicationSent'))
-            )
-            print(f"Application for {self.job_title} sent successfully.")
-            self.data_dict["applied"] = True
+    def execute_chat_gpt_prompts(self):
+        start_tag = '<CHAT_GPT>'
+        end_tag = '</CHAT_GPT>'
+        for paragraph in self.document.paragraphs:
+            matches = re.findall(fr"{start_tag}(.*?){end_tag}", paragraph.text)
+            if matches:
+                for match in matches:
+                    for tag in [start_tag, end_tag]:
+                        match = match.replace(tag, "")
 
-        except NoSuchWindowException:
-            print("Reopening closed browser")
-            self.driver = webdriver.Chrome(service=ChromeService(), options=webdriver.ChromeOptions())
-
-        except MaxRetryError:
-            print("Too many retries, skipping")
+                    self.messages_label.config(text=f"Querying ChatGPT... ({match})")
+                    query_response = self.return_chat_gpt_response(match, self.job_details)
+                    placeholder_text = f"{start_tag}{match}{end_tag}"
+                    paragraph.text = paragraph.text.replace(placeholder_text, query_response, 1)
 
     def apply(self):
-        if (self.driver is not None) and (self.driver.service.process is not None):
-            print("User closed driver")
-            return
-        elif self.driver is None:
+        self.load_secrets()
+        if self.driver is None:
             self.driver = webdriver.Chrome(service=ChromeService(), options=webdriver.ChromeOptions())
 
         try:
@@ -199,11 +220,9 @@ class Seeker(object):
 
         self.read_visited_jobs()
         self.temporary_documents = []
-        is_agency = False
         self.company_name = listing.get("companyName")
 
         if self.company_name is None:
-            is_agency = True
             self.company_name = listing.get("advertiser").get('description')
 
         self.job_title = listing.get("title")
@@ -225,7 +244,7 @@ class Seeker(object):
 
         else:
             self.driver.get(self.job_url)
-            print(f"Got {self.job_url}")
+            self.messages_label.config(text=f"Got {self.job_url}")
 
             self.replacements = {
                 "COMPANY_NAME": self.company_name,
@@ -235,33 +254,28 @@ class Seeker(object):
 
             self.job_details = self.driver.find_element(By.CSS_SELECTOR, 'div[data-automation="jobAdDetails"]').text
 
-            if self.role == "chat_gpt_test":
-                self.query_chat_gpt()
 
             for document_type in ['cv', 'cover_letter']:
-                if is_agency:
-                    self.template_path = os.path.join("templates", f"{document_type}_angus_hunt_{self.role}_agency.docx")
-                else:
-                    self.template_path = os.path.join("templates", f"{document_type}_angus_hunt_{self.role}.docx")
-
+                self.document_type = document_type
+                self.template_path = self.temp_path_dict[document_type]
                 self.document = Document(self.template_path)
 
                 if document_type == 'cover_letter':
+                    self.execute_chat_gpt_prompts()
                     self.update_document()
 
-                self.save_document(document_type)
+                self.set_document_name()
+                self.save_document()
 
             try:
                 self.click_quick_apply()
-                self.wait_random()
-                self.complete_application()
 
             except TimeoutException:
-                print("No quick apply")
+                self.messages_label.config(text="No quick apply")
 
-            self.remove_temporary_files()
+            self.wait_random()
+            self.complete_application()
             self.save_visited_job()
-
 
 class Interface(Seeker):
     def __init__(self, root):
@@ -269,95 +283,128 @@ class Interface(Seeker):
         self.root.title("Seeker")
         self.driver = None
         self.temporary_documents = []
+        self.temp_path_dict = {}
 
         self.main_frame = ttk.Frame(root)
         self.main_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
 
+        self.name_label = ttk.Label(self.main_frame, text="Your name:")
+        self.name_label.grid(row=0, column=0, columnspan=2, pady=5, padx=5, sticky='E')
+        self.name_entry = ttk.Entry(self.main_frame)
+        self.name_entry.grid(row=0, column=2, columnspan=2, pady=5, padx=5, sticky='W')
+
         self.role_label = ttk.Label(self.main_frame, text="Role:")
-        self.role_label.grid(row=0, column=0, columnspan=2, pady=5, padx=5, sticky='E')
-        role_options = ['gis', 'developer']
+        self.role_label.grid(row=1, column=0, columnspan=2, pady=5, padx=5, sticky='E')
+        role_options = ['gis', 'developer', 'retail assistant']
         self.role_dropdown = ttk.Combobox(self.main_frame, textvariable=tk.StringVar(), values=role_options)
         self.role_dropdown.set(role_options[1])
-        self.role_dropdown.grid(row=0, column=2, columnspan=2, pady=5, padx=5, sticky='W')
+        self.role_dropdown.grid(row=1, column=2, columnspan=2, pady=5, padx=5, sticky='W')
 
-        self.temp_path_label = ttk.Label(self.main_frame, text="Temporary Path:")
-        self.temp_path_label.grid(row=1, column=0, columnspan=2, pady=5, padx=5, sticky='E')
-        self.browse_button = ttk.Button(self.main_frame, text="Browse", command=self.browse_temp_path)
-        self.browse_button.grid(row=1, column=2, columnspan=2, pady=5, padx=5, sticky='W')
+        self.cv_template_label = ttk.Label(self.main_frame, text="CV Template:")
+        self.cv_template_label.grid(row=2, column=0, columnspan=2, pady=5, padx=5, sticky='E')
+        self.cv_template_button = ttk.Button(self.main_frame, text="Browse", command= lambda: self.browse_temp_path('cv'))
+        self.cv_template_button.grid(row=2, column=2, columnspan=2, pady=5, padx=5, sticky='W')
+
+        self.cover_letter_template_label = ttk.Label(self.main_frame, text="Cover Letter Template:")
+        self.cover_letter_template_label.grid(row=3, column=0, columnspan=2, pady=5, padx=5, sticky='E')
+        self.cover_letter_template_button = ttk.Button(self.main_frame, text="Browse", command= lambda: self.browse_temp_path('cover_letter'))
+        self.cover_letter_template_button.grid(row=3, column=2, columnspan=2, pady=5, padx=5, sticky='W')
 
         self.where_label = ttk.Label(self.main_frame, text="Where:")
-        self.where_label.grid(row=2, column=0, columnspan=2, pady=5, padx=5, sticky='E')
+        self.where_label.grid(row=4, column=0, columnspan=2, pady=5, padx=5, sticky='E')
         self.where_entry = ttk.Entry(self.main_frame)
-        self.where_entry.grid(row=2, column=2, columnspan=2, pady=5, padx=5, sticky='W')
+        self.where_entry.grid(row=4, column=2, columnspan=2, pady=5, padx=5, sticky='W')
 
         self.keywords_label = ttk.Label(self.main_frame, text="Keywords:")
-        self.keywords_label.grid(row=3, column=0, columnspan=2, pady=5, padx=5, sticky='E')
+        self.keywords_label.grid(row=5, column=0, columnspan=2, pady=5, padx=5, sticky='E')
         self.keywords_entry = ttk.Entry(self.main_frame)
-        self.keywords_entry.grid(row=3, column=2, columnspan=2, pady=5, padx=5, sticky='W')
+        self.keywords_entry.grid(row=5, column=2, columnspan=2, pady=5, padx=5, sticky='W')
 
         self.view_previous_jobs_check = ttk.Checkbutton(self.main_frame, text='View previously-skipped jobs', variable=tk.StringVar(), onvalue=True, offvalue=False)
-        self.view_previous_jobs_check.grid(row=4, column=0, columnspan=4, pady=5)
+        self.view_previous_jobs_check.grid(row=6, column=0, columnspan=4, pady=5)
         self.view_previous_jobs_check.invoke()
 
         self.run_button = ttk.Button(self.main_frame, text="Run", command=self.run_action)
-        self.run_button.grid(row=5, column=1, columnspan=2, pady=5)
+        self.run_button.grid(row=7, column=1, columnspan=2, pady=5)
 
         self.skip_button = ttk.Button(self.main_frame, text="Skip current listing", command=self.skip_action, state=DISABLED)
-        self.skip_button.grid(row=6, column=1, columnspan=2, pady=5)
+        self.skip_button.grid(row=8, column=1, columnspan=2, pady=5)
 
         self.quit_button = ttk.Button(self.main_frame, text="Quit", command=self.quit_action)
-        self.quit_button.grid(row=7, column=1, columnspan=2, pady=5)
+        self.quit_button.grid(row=9, column=1, columnspan=2, pady=5)
 
         self.messages_label = ttk.Label(self.main_frame, text="", wraplength=240, justify='center')
         self.messages_label.configure(anchor="center")
-        self.messages_label.grid(row=8, column=1, columnspan=2, pady=5)
+        self.messages_label.grid(row=10, column=0, columnspan=4, pady=5)
 
         self.temp_path_output = None
         self.thread = None
+        self.skip_clicked = False
 
-    def browse_temp_path(self):
-        temp_path = filedialog.askdirectory(title="Select Temporary Folder")
-        self.temp_path_output = temp_path
-        self.messages_label.config(text=f"Set temporary path to {temp_path}")
+    def browse_temp_path(self, document_type):
+        document_type_clean = document_type.replace("_", ' ')
+        temp_path = filedialog.askopenfilename(title=f"Select temporary {document_type_clean} path")
+        self.temp_path_dict[document_type] = temp_path
+        self.messages_label.config(text=f"Set temporary {document_type_clean} path to {temp_path}")
 
     @staticmethod
     def return_clean_url_args(url_args):
         return url_args.replace(' ', '+')
 
+    def start_thread(self):
+        self.thread = threading.Thread(target=self.apply)
+        self.thread.daemon = True
+        self.thread.start()
+        print("Thread started")
+
+    def restart_thread(self):
+        if self.thread is not None:
+            self.thread.join()  # Wait for the thread to finish
+            print("Thread stopped")
+        self.start_thread()
+
     def skip_action(self):
         print(f"Skipping {self.job_title}")
-        self.save_visited_job()
-        self.remove_temporary_files()
-        threading.Thread(target=self.apply).start()
+        self.skip_clicked = True
 
     def quit_action(self):
         if self.driver is not None:
             self.driver.quit()
+            self.thread.join()
         self.remove_temporary_files()
         sys.exit()
 
+    @staticmethod
+    def return_clean_text_input(text):
+        return text.strip().replace(" ", "_")
+
     def run_action(self):
-        self.role = self.role_dropdown.get()
+        self.check_state()
+        self.run_button['state'] = 'disabled'
+        self.name = self.return_clean_text_input(self.name_entry.get())
+        self.role = self.return_clean_text_input(self.role_dropdown.get())
         self.temp_path = self.temp_path_output
         self.where = self.return_clean_url_args(self.where_entry.get())
         self.keywords = self.return_clean_url_args(self.keywords_entry.get())
-        super().__init__(self.role, self.temp_path, self.where, self.keywords, self.view_previous_jobs_check)
+        super().__init__(self.name, self.role, self.temp_path_dict, self.where, self.keywords, self.view_previous_jobs_check)
 
         self.load_pages(max_pages=2)
         self.skip_button['state'] = NORMAL
         self.messages_label.config(text="Running browser...")
-        self.thread = threading.Thread(target=self.apply)
-        self.thread.daemon = True
-        self.thread.start()
+        self.start_thread()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = Interface(root)
-    app.role_dropdown.set('developer')
-    app.temp_path_output = r"C:\Users\angus\Documents\CV_Creator_Reborn\temporary"
+    app.name_entry.insert(0, "Juan Carlos")
+    app.role_dropdown.set('retail assistant')
+    app.temp_path_dict = {
+        "cv": r"templates/cv_juan_carlos.docx",
+        "cover_letter": r"templates/cover_letter_juan_carlos.docx"
+    }
     app.where_entry.insert(0, "All New Zealand")
-    app.keywords_entry.insert(0, "Python Developer")
+    app.keywords_entry.insert(0, "Customer service")
     test_path = r"C:\Users\angus\Documents\CV_Creator_Reborn\temporary"
     app.messages_label.config(text=f'Set temporary path to {test_path}')
     app.root.mainloop()
